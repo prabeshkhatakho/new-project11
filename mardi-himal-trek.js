1cb1b740d88b945a62ce8dc65bc65f6a6d7b95ee
Bibek Khatako <bibekkhatako@Bibeks-MBP.lan>
2023-05-07 20:10:16 -0400

    used config file to generate js files

---
+++ mardi-himal-trek.js
@@ -0,0 +1,20 @@
+
+  import React from "react"
+
+  function washingmachine() {
+    return (
+      <div>
+        
+<h1>Give detailed information about different types of washing machine</h1>
+<p>response</p>
+<h1>what is the cheapest washing machine</h1>
+<p>response</p>
+<h1>question number 3</h1>
+<p>response</p>
+      </div>
+    )
+  }
+
+  export default mardi-himal-trek;
+
+  
---
+++ programming-language.js
@@ -0,0 +1,20 @@
+
+  import React from "react"
+
+  function washingmachine() {
+    return (
+      <div>
+        
+<h1>Q1</h1>
+<p>response</p>
+<h1>Q2</h1>
+<p>response</p>
+<h1>Q3</h1>
+<p>response</p>
+      </div>
+    )
+  }
+
+  export default programming-language;
+
+  
